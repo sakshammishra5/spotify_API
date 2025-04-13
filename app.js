e6e7abd73ee24f207744f87c0879311396b465ea
sakshammishraGla <sakshammishra5thsep@gmail.com>
2025-04-13 13:22:43 +0530

    changes

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
 
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://saksham-portfolio-kappa.vercel.app/',
+}));
+
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
